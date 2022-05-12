8e55706a6294bf56e7dd79d42a6f7eca309ca737
Corey Willinger <corey.willinger@gmail.com>
2022-05-12 07:21:06 -0400

    Initial Commit

---
+++ app.js
@@ -0,0 +1,33 @@
+//jshint esversion:6
+
+//server configurations
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const app = express();
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+app.listen(3000, function() {
+  console.log("Server started on port 3000");
+});
+
+//Global variables
+const homeStartingContent = ""
+const aboutContent = ""
+const contactContent = ""
+
+//Routes
+
+
+
+
+
+
+
+
+
+
+
+
